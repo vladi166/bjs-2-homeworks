7909a7ab9dcfc59861587cdf9e983a9c7cdbaae3
Vladislav Andreev <vladireft96@mail.ru>
2025-04-11 12:50:59 +0500

    HW#2 Fix1 Ready

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,92 +1,92 @@
 function getArrayParams(...arr) {
 	if (arr.length === 0) {
-    return 0;
-  }
-  let min = arr[0];
-  let max = arr[0];
-  let sum = 0;
-  let avg = 0;
-  for (i = 0; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-    }
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-    sum = sum + arr[i];
-    avg = sum / arr.length;
-  }
+		return 0;
+	}
+	let min = arr[0];
+	let max = arr[0];
+	let sum = 0;
+	let avg = 0;
+	for (i = 0; i < arr.length; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		sum = sum + arr[i];
+		avg = sum / arr.length;
+	}
 
-  return {
-    min: min,
-    max: max,
-    avg: +(avg.toFixed(2))
-  }
+	return {
+		min: min,
+		max: max,
+		avg: +(avg.toFixed(2))
+	}
 }
 
 getArrayParams(10, 10, 11, 20, 10);
 
 function summElementsWorker(...arr) {
-  if (arr.length === 0) return 0;
+	if (arr.length === 0) return 0;
 
-  return arr.reduce((sum, num) => sum + num, 0);
+	return arr.reduce((sum, num) => sum + num, 0);
 }
 
 // Насадка вычисления разницы максимального и минимального элементов
 function differenceMaxMinWorker(...arr) {
-  if (arr.length === 0) return 0;
+	if (arr.length === 0) return 0;
 
-  const max = Math.max(...arr);
-  const min = Math.min(...arr);
-  return max - min;
+	const max = Math.max(...arr);
+	const min = Math.min(...arr);
+	return max - min;
 }
 
 // Насадка вычисления разницы сумм чётных и нечётных элементов
 function differenceEvenOddWorker(...arr) {
-  if (arr.length === 0) return 0;
+	if (arr.length === 0) return 0;
 
-  let sumEvenElement = 0;
-  let sumOddElement = 0;
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
 
-  for (let num of arr) {
-    if (num % 2 === 0) {
-      sumEvenElement += num;
-    } else {
-      sumOddElement += num;
-    }
-  }
+	for (let num of arr) {
+		if (num % 2 === 0) {
+			sumEvenElement += num;
+		} else {
+			sumOddElement += num;
+		}
+	}
 
-  return sumEvenElement - sumOddElement;
+	return sumEvenElement - sumOddElement;
 }
 
 // Насадка вычисления среднего значения чётных элементов
 function averageEvenElementsWorker(...arr) {
-  if (arr.length === 0) return 0;
+	if (arr.length === 0) return 0;
 
-  let sumEvenElement = 0;
-  let countEvenElement = 0;
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
 
-  for (let num of arr) {
-    if (num % 2 === 0) {
-      sumEvenElement += num;
-      countEvenElement++;
-    }
-  }
+	for (let num of arr) {
+		if (num % 2 === 0) {
+			sumEvenElement += num;
+			countEvenElement++;
+		}
+	}
 
-  return countEvenElement === 0 ? 0 : sumEvenElement / countEvenElement;
+	return countEvenElement === 0 ? 0 : sumEvenElement / countEvenElement;
 }
 
 function makeWork(arrOfArr, func) {
-  let maxWorkerResult = -Infinity;
+	let maxWorkerResult = -Infinity;
 
-  for (let arr of arrOfArr) {
-    const result = func(...arr);
-    if (result > maxWorkerResult) {
-      maxWorkerResult = result;
-    }
-  }
+	for (let arr of arrOfArr) {
+		const result = func(...arr);
+		if (result > maxWorkerResult) {
+			maxWorkerResult = result;
+		}
+	}
 
-  return maxWorkerResult;
+	return maxWorkerResult;
 }
 
 const arr = [
